a919cc6e8e2db8f2a5a603f1f8fd9a3fdf897756
Stanislav Vaštyl <stanislav@vastyl.cz>
2014-05-16 10:04:47 +0200

    Update list_of_small.pl

--- list_of_small.pl
+++ list_of_small.pl
@@ -22,10 +22,12 @@
 use strict;
 use warnings;
 
+our $total_war=0;
+our $total_err=0;
 ### mail settings
-my $to = 'xxxxx';
-my $from = 'xxxxx';
-my $subject = 'xxxxx';
+my $to = 'dataprotector@vsb.cz';
+my $from = 'jelen';
+my $subject = 'Homen - count small files';
 
 ### list of directory root
 my $root = "/";
@@ -38,6 +40,7 @@ my $sum_cf = 0;
 my $sum_csf = 0;
 foreach my $item (@dirs) {
 	my $percent = 0;
+	my $status = "";
 	my $cf = `find $root$item  -type f |wc -l 2>/dev/null`;
 	my $csf =`find $root$item -type f -size -1024c |wc -l 2>/dev/null`;
 	chomp($csf); chomp($cf);
@@ -46,9 +49,12 @@ foreach my $item (@dirs) {
 	if ($csf != 0){
 		$percent = int(($csf / $cf) * 100);
 	} 
-	push(@result, "$root$item \t $percent %\nSmall files \t $csf \nTotal files \t $cf \n***---***---***\n");
+	if ($percent>=50 && $percent<90) {$status=" - WARNING!"; $total_war=$total_war+1;}
+	elsif ($percent>=90) {$status=" - CRITICAL!"; $total_err=$total_err+1;}
+	push(@result, "$root$item \t $percent% $status\nSmall files \t $csf \nTotal files \t $cf \n***---***---***\n");
 	
 }
+
 open MAIL, "|/usr/sbin/sendmail -t";
 
 ### Mail Header
@@ -56,16 +62,18 @@ print MAIL "To: $to\n";
 print MAIL "From: $from\n";
 print MAIL "Subject: $subject\n\n";
 ### Mail Body
-print MAIL "RESULT of small files in server\n";
+print MAIL "\nRESULT of small files in server\n";
 print MAIL "-------------------------------\n";
 print MAIL @result;
-print MAIL "\n\n";
+print MAIL "\n";
 print MAIL "Total files is: $sum_cf \n";
 print MAIL "Total small files is: $sum_csf \n";
 my $sum_percent = 0;
 if ($sum_csf != 0){
 	$sum_percent = int(($sum_csf / $sum_cf) * 100);
 }
-print MAIL "Total average is: $sum_percent %";
+print MAIL "Total average is: $sum_percent %\n";
+print MAIL "Total warning is: $total_war\n";
+print MAIL "Total error is: $total_err";
 
 close MAIL;
